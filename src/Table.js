83eb7e400ee3da8eac7557cbff80e46175085f38
arye <aryef2@hotmail.com>
2020-12-06 14:20:02 +0200

    use tableBody class instead of const

--- src/Table.js
+++ src/Table.js
@@ -24,6 +24,24 @@ const TableBody = (props1) => {
     return <tbody>{rows}</tbody>
   }
 
+  class TableBody1 extends Component {
+      render() {
+        const rows = this.props.characterData2.map((row, index) => {
+            return (
+              <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+              </tr>
+            )     
+          }            
+         )   
+         return (
+            <tbody>{rows}</tbody> 
+            )
+    
+  }
+}
+
 class Table extends Component {
   render() {
     //const {characterData1} = this.props
@@ -31,7 +49,7 @@ class Table extends Component {
     return (
         <table>
         <TableHeader />
-        <TableBody characterData2 ={characterData11} />
+        <TableBody1 characterData2 ={characterData11} />
       </table>
     )
   }
